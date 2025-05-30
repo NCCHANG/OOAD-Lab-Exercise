b7442dcf90f04717e82518d040d17c13efba3c59
Lemuel Goh <chunyong2008@hotmail.com>
2025-05-30 17:46:37 +0800

    refactor: enhance ColourSelectionButton to manage color selection and feedback

--- ColourSelectionButton.java
+++ ColourSelectionButton.java
@@ -1,5 +1,29 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ColourSelectionButton extends JButton {
-    
+
+    private Color selectedColor;
+
+    public ColourSelectionButton(String label) {
+        super(label);
+        selectedColor = Color.BLACK; // default color
+
+        this.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Color newColor = JColorChooser.showDialog(null, "Choose a Color", selectedColor);
+                if (newColor != null) {
+                    selectedColor = newColor;
+                    setBackground(selectedColor); // update button color for feedback
+                }
+            }
+        });
+    }
+
+    public Color getSelectedColor() {
+        return selectedColor;
+    }
 }
